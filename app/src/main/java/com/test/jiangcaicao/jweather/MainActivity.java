f0979657f6363b1ed0dbefafadf65dc70fe6f9c0
jiangcaicao <jiangcaicao@126.com>
2018-04-13 16:03:16 +0800

    Initial commit

---
+++ app/src/main/java/com/test/jiangcaicao/jweather/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.jiangcaicao.jweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sunbaichuan on 4/13/18.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
